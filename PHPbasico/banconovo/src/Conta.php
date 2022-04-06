ce945ce9cc8728d1b6960b7b15b5ec19da20adcf
Edelberto Rösler <edelberto.rosler@sensedia.com>
2022-04-06 09:24:34 -0300

    Criado método depositar

--- PHPbasico/banconovo/src/Conta.php
+++ PHPbasico/banconovo/src/Conta.php
@@ -7,13 +7,22 @@ class Conta
     public $saldo;
 
 
-    public function sacar(float $valor)
+    public function sacar(float $valor): void
     {
         if ($valor > 0 && $valor < $this->saldo) {
             $this->saldo -= $valor;
         }
         else {
             echo "Impossível realizar o saque!";
+        }
+    }
+
+    public function depositar(float $valor): void 
+    {
+        if ($valor > 0) {
+            $this->saldo += $valor;
+        } else{
+            echo "Impossível realizar o depósito";
         }
     }
 
