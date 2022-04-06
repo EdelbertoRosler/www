337a4f79fbfb7d602066ba97dad2f2fb7dda7c8a
Edelberto Rösler <edelberto.rosler@sensedia.com>
2022-04-06 10:35:08 -0300

    Criado o método transferir e ajustado o comportamento dos outros métodos

--- PHPbasico/banconovo/src/Conta.php
+++ PHPbasico/banconovo/src/Conta.php
@@ -9,23 +9,31 @@ class Conta
 
     public function sacar(float $valor): void
     {
-        if ($valor > 0 && $valor < $this->saldo) {
-            $this->saldo -= $valor;
-        }
-        else {
+        if ($valor <= 0 || $valor > $this->saldo) {
             echo "Impossível realizar o saque!";
+            return;
         }
+        $this->saldo -= $valor;
     }
 
     public function depositar(float $valor): void 
     {
-        if ($valor > 0) {
-            $this->saldo += $valor;
-        } else{
+        if ($valor <= 0) {
             echo "Impossível realizar o depósito";
+            return;   
         }
+        $this->saldo += $valor;
     }
 
+    public function transferir(float $valor, Conta $conta): void
+    {
+        if ($valor <= 0 || $valor > $this->saldo) {    
+            echo "Impossível realizar a transferência";
+            return;
+        }
+        $this->sacar($valor);
+        $conta->depositar($valor);
+    }
 }
 
 
--- PHPbasico/banconovo/src/banco.php
+++ PHPbasico/banconovo/src/banco.php
@@ -2,11 +2,32 @@
 include_once "Conta.php";
 
 $conta1 = new Conta();
+$conta2 = new Conta();
 
 $conta1->cpf = "001.001.001-11";
 $conta1->titular = "Gabriel Rocha";
 $conta1->saldo = 1000;
 
-$conta1->sacar(100);
-var_dump($conta1);
+$conta2->cpf = "002.002.002-22";
+$conta2->titular = "Tiago Costa";
+$conta2->saldo = 7000;
 
+// -------------------------------
+
+$conta1->sacar(100);
+$conta1->depositar(300);
+$conta1->transferir(100, $conta2);
+
+$conta2->sacar(500);
+$conta2->depositar(100);
+$conta2->transferir(600, $conta1);
+
+
+var_dump($conta1);
+echo "<br>";
+var_dump($conta2);
+
+
+
+
+// var_dump($conta1);
