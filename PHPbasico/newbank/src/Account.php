fde66daeb22f54fab8afa4c5b561c64de7c3a8f5
Edelberto Rösler <edelberto.rosler@sensedia.com>
2022-04-07 00:16:51 -0300

    Criando getters, construtor, destrutor e métodos estáticos

--- PHPbasico/newbank/src/Account.php
+++ PHPbasico/newbank/src/Account.php
@@ -1,12 +1,27 @@
 <?php
 
-class Account 
+class Account                                                   // Evitar escrever atributos públicos
 {
     private $cpf;
     private $holder;
     private $balance;
+    private static $countAccounts = 0;                          // É um membro da classe e não de um objeto que será criado na classe. Evitar esse tipo de abordagem se não for realmente necessário
 
+    // Constutor - con
+    public function __construct(string $cpf, string $holder) {
+        $this->cpf = $cpf;
+        $this->validateHolder($holder);
+        $this->balance = 0;
+        self::$countAccounts ++;                                // O self e os dois pontos é a atribuição (self substitui "this", : substitui "->")
+    }                                                           // poderia usar "Account::countAccounts ++" o resultado seria o mesmo
 
+    // Destruct
+    public function __destruct()                                // Para apagar da memória os objetos obsoletos 
+    {
+        self::$countAccounts --;
+    }
+
+    // Funções - fun
     public function whithdraw(float $value): void
     {
         if ($value <= 0 || $value > $this->balance) {
@@ -25,30 +40,44 @@ class Account
         $this->balance += $value;
     }
 
-    public function transfer(float $value, Conta $conta): void
+    public function transfer(float $value, Account $account): void
     {
         if ($value <= 0 || $value > $this->balance) {    
             echo "Impossível realizar a transferência";
             return;
         }
         $this->whithdraw($value);
-        $conta->deposit($value);
+        $account->deposit($value);
     }
 
-
-    public function getName(string $name): string
+    private function validateHolder(string $holder)             // validação para o nome
     {
-        return $this->$name;
+        if (strlen($holder) < 5) {
+            echo "O nome deve conter ao menos 5 caracteres!";   // strlen = número de caracteres
+            exit;
+        }
+        $this->holder = $holder;
     }
 
-    public function getcpf(string $cpf): string
+    // Getters
+    public function getName(): string
     {
-        return $this->$cpf;
+        return $this->holder;
     }
-    
-    public function getSaldo(): float
+
+    public function getcpf(): string
     {
-        return $this->$balance;
+        return $this->cpf;
+    }
+
+    public function getBalance(): float
+    {
+        return $this->balance;
+    }
+
+    public static function getCountAccounts(): int              // Para poder acessar fora da classe, cria um getter estático
+    {
+        return self::$countAccounts;
     }
 
 
--- PHPbasico/newbank/src/bank.php
+++ PHPbasico/newbank/src/bank.php
@@ -1,33 +1,29 @@
 <?php
 include_once "Account.php";
 
-$account1 = new Account();
-$account2 = new Account();
+$account1 = new Account("001.001.001-11", "Gabriel Rocha");
+$account2 = new Account("002.002.002-22", "Tiago Costa");
 
-$account1->cpf = "001.001.001-11";
-$account1->titular = "Gabriel Rocha";
-$account1->saldo = 1000;
-
-$account2->cpf = "002.002.002-22";
-$account2->titular = "Tiago Costa";
-$account2->saldo = 7000;
 
 // -------------------------------
 
+$account1->deposit(1000);
 $account1->whithdraw(100);
-$account1->deposit(300);
-$account1->transfer(100, $account2);
+$account1->transfer(200, $account2);
 
+$account2->deposit(8000);
 $account2->whithdraw(500);
-$account2->deposit(100);
 $account2->transfer(600, $account1);
 
 
-var_dump($account1);
+var_dump("account 1 R$ " . $account1->getBalance());
 echo "<br>";
-var_dump($account2);
+var_dump("account 2 R$ " . $account2->getBalance());
+echo "<br>";
+var_dump($account1->getName());
+echo "<br>";
+
+echo Account::getCountAccounts();
 
 
 
-
-// var_dump($account1);
